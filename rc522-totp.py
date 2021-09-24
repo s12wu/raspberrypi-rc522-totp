c2b72fadeb38e11f5574a931c287a1b388cd5c6f
Sören <12wu@gmx.de>
2021-09-24 14:20:03 +0200

    removed small window with only a read button -> show desktop notification instead

--- rc522-totp.py
+++ rc522-totp.py
@@ -3,6 +3,8 @@ from mfrc522 import SimpleMFRC522
 
 import pyotp
 
+import notify2
+
 from tkinter import *
 
 #service names for the totp labels - make sure they are the right number and in the same order like the secrets on the rfid tag!
@@ -15,30 +17,15 @@ def readTag():
     text = text.split(".")
     return text
 
-#list of tkinter label objects showing the service name with totp code
-labels = []
 
-# True after first read - no need th re-draw warning then
-warningShown = False
-
-def createCodes():
+def createCodes(secrets):
     global warningShown, labels
 
-    #remove old codes from the window
-    for i in range(len(labels)):
-        labels[i].grid_forget()
-
-    labels = []
-
     #show a warning
-    if not warningShown:
-        warningShown = True
-        Label(main, text="Attention: Since the codes are not automatically regenerated,", font=("sans-serif", 8)).grid(row=1, column=0)
-        Label(main, text="they will be invalid after a maximum of 30 seconds.", font=("sans-serif", 8)).grid(row=2, column=0)
+    Label(main, text="Attention: Since the codes are not automatically regenerated,", font=("sans-serif", 8)).grid(row=0, column=0)
+    Label(main, text="they will be invalid after a maximum of 30 seconds.", font=("sans-serif", 8)).grid(row=1, column=0)
     
-    
-    secrets = readTag();
-    
+        
     for i in range(len(names)):
         totp = pyotp.TOTP(secrets[i])
         code = totp.now()
@@ -47,17 +34,30 @@ def createCodes():
         print(labelText)
 
         lbl = Label(main, text=labelText, font=("sans-serif", 30))
-        lbl.grid(row=3+i, column=0)
+        lbl.grid(row=2+i, column=0)
 
 
 
 reader = SimpleMFRC522()
-main = Tk()
-main.title("Raspberry Pi RFID TOTP code generator")
+notify2.init("Raspberry Pi RFID TOTP code generator")
+
+n = notify2.Notification("Raspberry Pi RFID TOTP code generator",
+                         "please scan your tag to generate codes",
+                         "notification-message-im"
+                        )
 
 
 try:
-    Button(main, text="READ", command=createCodes).grid(row=0, column=0)
+    n.show()
+    
+    secrets = readTag()
+
+    #show tkinter window with codes
+    main = Tk()
+    main.title("Raspberry Pi RFID TOTP code generator")
+
+    createCodes(secrets)
+
     main.mainloop()
 
 finally:
